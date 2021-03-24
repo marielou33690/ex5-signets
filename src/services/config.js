27a83cf33efe04ad389ee8b8df72121da29480d1
Marie-Lou Denis <marielou33690@gmail.com>
2021-03-24 13:21:04 -0400

    premier commit

---
+++ src/services/config.js
@@ -0,0 +1,15 @@
+/*
+  Ce fichier contient les variables de configuration
+  reliées à Firebase et ses produits.
+*/
+
+// Configuration Firebase (mettez-y les vôtres !)
+const firebaseConfig = {
+  
+};
+export default firebaseConfig;
+
+// Nom de la collection principale
+export const utilRef = "utilisateurs-ex5";
+// Nom de la sous-collection
+export const dossRef = "dossiers";
